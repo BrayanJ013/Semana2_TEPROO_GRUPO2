346ea176fb7d1fb4747c4da2dc79b9abca1eb1c4
Brayan <n00459247@upn.pe>
2026-08-27 17:05:53 -0500

    primer avance

---
+++ Caso4/src/clase/TrianguloRectangulo.java
@@ -0,0 +1,27 @@
+package clase;
+
+public class TrianguloRectangulo {
+private double cateto1, cateto2;
+
+public TrianguloRectangulo(double cateto1, double cateto2) {
+	this.cateto1 = cateto1;
+	this.cateto2 = cateto2;
+}
+
+public double getCateto1() {
+	return cateto1;
+}
+
+public void setCateto1(double cateto1) {
+	this.cateto1 = cateto1;
+}
+
+public double getCateto2() {
+	return cateto2;
+}
+
+public void setCateto2(double cateto2) {
+	this.cateto2 = cateto2;
+}	
+
+}
