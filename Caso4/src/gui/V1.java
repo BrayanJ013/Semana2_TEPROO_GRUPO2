b03f510c08504c8196c26f6e0d3e46ba145d37d5
Brayan <n00459247@upn.pe>
2026-08-27 19:36:19 -0500

    boton procesar completo

--- Caso4/src/gui/V1.java
+++ Caso4/src/gui/V1.java
@@ -6,12 +6,17 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+
+import clase.TrianguloRectangulo;
+
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 import javax.swing.JButton;
 import javax.swing.JTextArea;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
-public class V1 extends JFrame {
+public class V1 extends JFrame implements ActionListener {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
@@ -72,7 +77,8 @@ public class V1 extends JFrame {
 		}
 		{
 			btnNewButton = new JButton("Procesar");
-			btnNewButton.setBounds(243, 36, 84, 20);
+			btnNewButton.addActionListener(this);
+			btnNewButton.setBounds(198, 22, 84, 20);
 			contentPane.add(btnNewButton);
 		}
 		{
@@ -81,5 +87,23 @@ public class V1 extends JFrame {
 			contentPane.add(txtS);
 		}
 
+	}
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == btnNewButton) {
+			do_btnNewButton_actionPerformed(e);
+		}
+	}
+	protected void do_btnNewButton_actionPerformed(ActionEvent e) {
+	double cateto1=Double.parseDouble(txtCat1.getText());
+	double cateto2=Double.parseDouble(txtCat2.getText());
+	TrianguloRectangulo t=new TrianguloRectangulo(cateto1, cateto2);
+	Imprimir("El cateto 1 es: "+t.getCateto1());
+	Imprimir("El cateto 2 es: "+t.getCateto2());
+	Imprimir("El area es: "+t.area());
+	Imprimir("La hipotenusa 1 es: "+t.hipotenusa());
+	Imprimir("El perimetro es: "+t.perimetro());
+	}
+	void Imprimir (String s) {
+		txtS.append(s+"\n");
 	}
 }
