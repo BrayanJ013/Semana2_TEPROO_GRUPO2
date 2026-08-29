0462ba60d5e159fc77dc24df6f3ee57993270000
duegipe <goldmandiaz@gmail.com>
2026-08-29 04:48:58 -0500

    agregar boton limpiar

--- Caso4/src/gui/V1.java
+++ Caso4/src/gui/V1.java
@@ -25,6 +25,8 @@ public class V1 extends JFrame implements ActionListener {
 	private JTextField txtCat1;
 	private JTextField txtCat2;
 	private JButton btnNewButton;
+	private JButton btnHipotenusa;
+	private JButton btnLimpiar;
 	private JTextArea txtS;
 
 	/**
@@ -55,31 +57,43 @@ public class V1 extends JFrame implements ActionListener {
 		contentPane.setLayout(null);
 		{
 			lblNewLabel = new JLabel("Cateto 1:");
-			lblNewLabel.setBounds(10, 26, 44, 12);
+			lblNewLabel.setBounds(10, 26, 60, 12);
 			contentPane.add(lblNewLabel);
 		}
 		{
 			lblNewLabel_1 = new JLabel("Cateto 2:");
-			lblNewLabel_1.setBounds(10, 58, 44, 12);
+			lblNewLabel_1.setBounds(10, 58, 60, 12);
 			contentPane.add(lblNewLabel_1);
 		}
 		{
 			txtCat1 = new JTextField();
-			txtCat1.setBounds(64, 23, 96, 18);
+			txtCat1.setBounds(75, 23, 96, 18);
 			contentPane.add(txtCat1);
 			txtCat1.setColumns(10);
 		}
 		{
 			txtCat2 = new JTextField();
-			txtCat2.setBounds(64, 55, 96, 18);
+			txtCat2.setBounds(75, 55, 96, 18);
 			contentPane.add(txtCat2);
 			txtCat2.setColumns(10);
 		}
 		{
 			btnNewButton = new JButton("Procesar");
 			btnNewButton.addActionListener(this);
-			btnNewButton.setBounds(198, 22, 84, 20);
+			btnNewButton.setBounds(198, 22, 100, 25);
 			contentPane.add(btnNewButton);
+		}
+		{
+			btnHipotenusa = new JButton("Hipotenusa");
+			btnHipotenusa.addActionListener(this);
+			btnHipotenusa.setBounds(198, 55, 100, 25);
+			contentPane.add(btnHipotenusa);
+		}
+		{
+			btnLimpiar = new JButton("Limpiar");
+			btnLimpiar.addActionListener(this);
+			btnLimpiar.setBounds(304, 22, 100, 25);
+			contentPane.add(btnLimpiar);
 		}
 		{
 			txtS = new JTextArea();
@@ -92,6 +106,23 @@ public class V1 extends JFrame implements ActionListener {
 		if (e.getSource() == btnNewButton) {
 			do_btnNewButton_actionPerformed(e);
 		}
+		if (e.getSource() == btnHipotenusa) {
+			do_btnHipotenusa_actionPerformed(e);
+		}
+		if (e.getSource() == btnLimpiar) {
+			do_btnLimpiar_actionPerformed(e);
+		}
+	}
+	protected void do_btnLimpiar_actionPerformed(ActionEvent e) {
+	txtCat1.setText("");
+	txtCat2.setText("");
+	txtS.setText("");
+	}
+	protected void do_btnHipotenusa_actionPerformed(ActionEvent e) {
+	double cateto1=Double.parseDouble(txtCat1.getText());
+	double cateto2=Double.parseDouble(txtCat2.getText());
+	TrianguloRectangulo t=new TrianguloRectangulo(cateto1, cateto2);
+	Imprimir("La hipotenusa es: "+t.hipotenusa());
 	}
 	protected void do_btnNewButton_actionPerformed(ActionEvent e) {
 	double cateto1=Double.parseDouble(txtCat1.getText());
@@ -106,4 +137,4 @@ public class V1 extends JFrame implements ActionListener {
 	void Imprimir (String s) {
 		txtS.append(s+"\n");
 	}
-}
+}	
